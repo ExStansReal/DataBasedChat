fb53c5ef6f9cedb7afa81f20ea84cc0987a1c8bc
ExStansReal <yurakoshke@mail.ru>
2022-05-17 09:39:33 +0300

    Окно авторизации готово, главное окно приложения внешне готово

--- DataBasedChat/MainWindow.xaml.cs
+++ DataBasedChat/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DataBasedChat.DataBasedChatDataSetTableAdapters;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +34,50 @@ namespace DataBasedChat
             a.Left = this.Left;
             this.Hide();
             a.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+            bool exists = false;
+            User outUser = new User();
+            for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+            {
+                User a = new User();
+                dataGrid.SelectedIndex = i;
+                string login = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[4].ToString();
+                string password = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[5].ToString();
+                if (log.Text == login && pas.Password == password)
+                {
+                    exists = true;
+
+                    a.Id = Convert.ToInt32((dataGrid.SelectedItem as DataRowView).Row.ItemArray[0].ToString());
+                    a.Familiya = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                    a.Ima = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                    a.Othcestvo = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                    a.Login = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[4].ToString();
+                    a.Password = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[5].ToString();
+
+                    outUser = a;
+                    break;
+                }
+            }
+            if (exists == true)
+            {
+                Messenger a = new Messenger(outUser);
+                a.Top = this.Top;
+                a.Left = this.Left;
+                this.Hide();
+                a.Show();
+            }
+            else
+                MessageBox.Show("Проверьте логин или пароль");
+
+
         }
     }
 }
---
+++ DataBasedChat/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBasedChat
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Ima { get; set; }
+        public string Familiya { get; set; }
+        public string Othcestvo { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+       
+        
+    }
+}
