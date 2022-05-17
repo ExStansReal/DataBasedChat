6b88ae1a480706c736f4a666486817c6594b7c23
ExStansReal <yurakoshke@mail.ru>
2022-05-17 13:49:31 +0300

    Готовая часть отправки заявлений в друзья. Разработка динамического изменения данных в ListView

--- DataBasedChat/DataBasedChatDataSet.Designer.cs
+++ DataBasedChat/DataBasedChatDataSet.Designer.cs
@@ -4356,11 +4356,20 @@ SELECT ID_Zayavki, User_ID_From, User_ID_To, Date FROM Zayavki WHERE (ID_Zayavki
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT ID_Zayavki, User_ID_From, User_ID_To, Date FROM dbo.Zayavki";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "INSERT INTO [dbo].[Zayavki] ([User_ID_From], [User_ID_To], [Date]) VALUES (@User_" +
+                "ID_From, @User_ID_To, @Date);\r\nSELECT ID_Zayavki, User_ID_From, User_ID_To, Date" +
+                " FROM Zayavki ";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@User_ID_From", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "User_ID_From", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@User_ID_To", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "User_ID_To", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Date", global::System.Data.SqlDbType.Date, 3, global::System.Data.ParameterDirection.Input, 0, 0, "Date", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -4500,6 +4509,37 @@ SELECT ID_Zayavki, User_ID_From, User_ID_To, Date FROM Zayavki WHERE (ID_Zayavki
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, true)]
         public virtual int Update(int User_ID_From, int User_ID_To, System.DateTime Date, int Original_ID_Zayavki, int Original_User_ID_From, int Original_User_ID_To, System.DateTime Original_Date) {
             return this.Update(User_ID_From, User_ID_To, Date, Original_ID_Zayavki, Original_User_ID_From, Original_User_ID_To, Original_Date, Original_ID_Zayavki);
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Insert, false)]
+        public virtual int InsertQuery(int User_ID_From, int User_ID_To, string Date) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[1];
+            command.Parameters[0].Value = ((int)(User_ID_From));
+            command.Parameters[1].Value = ((int)(User_ID_To));
+            if ((Date == null)) {
+                throw new global::System.ArgumentNullException("Date");
+            }
+            else {
+                command.Parameters[2].Value = ((string)(Date));
+            }
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
         }
     }
     
--- DataBasedChat/FindFriends.xaml.cs
+++ DataBasedChat/FindFriends.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DataBasedChat.DataBasedChatDataSetTableAdapters;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DataBasedChat
 {
@@ -20,11 +24,71 @@ namespace DataBasedChat
     public partial class FindFriends : Window
     {
         private User User;
+        DispatcherTimer Timer;
+        int oldCount = 0;
+        int NewCount = 0;
         public FindFriends(User user)
         {
             InitializeComponent();
             User = user;
+
+            GetFirstPeople();
+
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += GetPeople;
+            Timer.Start();
+
+
         }
+        private void GetFirstPeople()
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+
+            oldCount = dataGrid.Items.Count;
+            listPeople.Items.Clear();
+            for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+            {
+                dataGrid.SelectedIndex = i;
+                string fami = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                string imaa = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                string otches = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                string fio = fami + " " + imaa + " " + otches;
+                listPeople.Items.Add(fio);
+            }
+        }
+        
+        private void GetPeople(object sender, EventArgs e)
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+            NewCount = dataGrid.Items.Count;
+            if (NewCount != oldCount)
+            {
+                oldCount = NewCount;
+                listPeople.Items.Clear();
+                for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+                {
+                    dataGrid.SelectedIndex = i;
+                    string fami = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                    string imaa = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                    string otches = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                    string fio = fami + " " + imaa  + " "+ otches;
+                    listPeople.Items.Add(fio);
+                }
+            }
+            
+        }
+
+
 
         private void Back()
         {
@@ -37,6 +101,129 @@ namespace DataBasedChat
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Back();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (listPeople.SelectedItem != null)
+            {
+                if (CheckExistsZayavlenie() == false)
+                {
+                    if (CheckExistsFriend() == false)
+                    {
+                        new ZayavkiTableAdapter().InsertQuery(User.Id, GetIdUser(), Convert.ToString(DateTime.Today));
+                        MessageBox.Show("Заявление отправлено");
+                    }
+                    else
+                        MessageBox.Show("Вы уже в друзьях у этого человека");
+                }
+                else
+                    MessageBox.Show("Вы уже отправили заявление этому человеку");
+            }
+            else
+                MessageBox.Show("Сначала выберет пользователя");
+        }
+        private int GetIdUser()
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+            for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+            {
+                dataGrid.SelectedIndex = i;
+                string fami = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                string imaa = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                string otches = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                string fio = fami + " " + imaa + " " + otches;
+                if (fio == listPeople.SelectedItem.ToString())
+                {
+                    int id = Convert.ToInt32((dataGrid.SelectedItem as DataRowView).Row.ItemArray[0].ToString());
+
+                    return id;
+                }
+            }
+
+            return 0;
+        }
+
+        private bool CheckExistsZayavlenie()
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+            for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+            {
+                dataGrid.SelectedIndex = i;
+                string fami = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                string imaa = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                string otches = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                string fio = fami + " " + imaa + " " + otches;
+                if (fio == listPeople.SelectedItem.ToString())
+                {
+                    int id = Convert.ToInt32((dataGrid.SelectedItem as DataRowView).Row.ItemArray[0].ToString());
+
+                    DataGrid dataGrid1 = new DataGrid();
+                    ZayavkiTableAdapter adapter1 = new ZayavkiTableAdapter();
+                    DataBasedChatDataSet.ZayavkiDataTable table1 = new DataBasedChatDataSet.ZayavkiDataTable();
+                    adapter1.Fill(table1);
+                    dataGrid1.ItemsSource = table1;
+                    for (int j = 0; i < dataGrid1.Items.Count - 1; i++)
+                    {
+                        dataGrid1.SelectedIndex = j;
+
+                        int id1 = Convert.ToInt32((dataGrid1.SelectedItem as DataRowView).Row.ItemArray[2].ToString());
+                        if (id1 == id)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckExistsFriend()
+        {
+            DataGrid dataGrid = new DataGrid();
+            UserTableAdapter adapter = new UserTableAdapter();
+            DataBasedChatDataSet.UserDataTable table = new DataBasedChatDataSet.UserDataTable();
+            adapter.Fill(table);
+            dataGrid.ItemsSource = table;
+            for (int i = 0; i < dataGrid.Items.Count - 1; i++)
+            {
+                    dataGrid.SelectedIndex = i;
+                    string fami = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[1].ToString();
+                    string imaa = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[2].ToString();
+                    string otches = (dataGrid.SelectedItem as DataRowView).Row.ItemArray[3].ToString();
+                    string fio = fami + " " + imaa + " " + otches;
+                if(fio == listPeople.SelectedItem.ToString())
+                {
+                    int id = Convert.ToInt32((dataGrid.SelectedItem as DataRowView).Row.ItemArray[0].ToString());
+
+                    DataGrid dataGrid1 = new DataGrid();
+                    FriendsTableAdapter adapter1 = new FriendsTableAdapter();
+                    DataBasedChatDataSet.FriendsDataTable table1 = new DataBasedChatDataSet.FriendsDataTable();
+                    adapter1.Fill(table1);
+                    dataGrid1.ItemsSource = table1;
+                    for (int j = 0; i < dataGrid1.Items.Count - 1; i++)
+                    {
+                        dataGrid1.SelectedIndex = j;
+
+                        int id1 = Convert.ToInt32((dataGrid1.SelectedItem as DataRowView).Row.ItemArray[2].ToString());
+                        if(id1 == id)
+                        {
+                            return true;
+                        }
+                    }
+                }
+             }
+            
+            return false;
         }
     }
 }
